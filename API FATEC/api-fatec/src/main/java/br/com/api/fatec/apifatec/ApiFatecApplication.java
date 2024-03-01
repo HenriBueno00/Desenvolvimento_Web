eed4a8c106af682dd0bf48a40c4c83b6f9cf8831
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-02-29 22:25:47 -0300

    Update Cod

--- API FATEC/api-fatec/src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
+++ API FATEC/api-fatec/src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
@@ -2,10 +2,33 @@ package br.com.api.fatec.apifatec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 @SpringBootApplication
 public class ApiFatecApplication {
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World";
+	}
+	
+	@RequestMapping("/hello")
+	String home2() {
+		return "hello word 2";
+	}
+	@RequestMapping("/numero")
+	Integer home3() {
+		return 2;
 
+	}
+	@RequestMapping("/soma")
+	Double home4() {
+		return 2.2+2;
+
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFatecApplication.class, args);
 	}
