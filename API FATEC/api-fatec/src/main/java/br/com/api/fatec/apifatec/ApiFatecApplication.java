3b62b8239f2e2e617bd0da9de89c78c40a92fc70
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-02-29 22:36:01 -0300

    Update Cod

--- API FATEC/api-fatec/src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
+++ API FATEC/api-fatec/src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
@@ -2,8 +2,11 @@ package br.com.api.fatec.apifatec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
 @RestController
 @SpringBootApplication
 public class ApiFatecApplication {
@@ -17,7 +20,7 @@ public class ApiFatecApplication {
 	String home2() {
 		return "hello word 2";
 	}
-	@RequestMapping("/numero")
+	@RequestMapping("/teste")
 	Integer home3() {
 		return 2;
 
@@ -25,6 +28,11 @@ public class ApiFatecApplication {
 	@RequestMapping("/soma")
 	Double home4() {
 		return 2.2+2;
+
+	}
+	@RequestMapping("/numero/{num}")
+	Integer numero2(@PathVariable Integer num) {
+		return num;
 
 	}
 	
