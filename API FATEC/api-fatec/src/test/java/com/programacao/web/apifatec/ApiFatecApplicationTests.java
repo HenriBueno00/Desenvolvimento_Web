2e8a97fa49b270a8c6788ff64090c376fa967a34
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-02-29 21:00:28 -0300

    Update

---
+++ API FATEC/api-fatec/src/test/java/com/programacao/web/apifatec/ApiFatecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.programacao.web.apifatec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFatecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
