e381298fa133ef1331c132ed8ce1bf2952e6d7e6
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-03-06 19:31:26 -0300

    Update arquivo Exercicio 01

--- API FATEC/api-fatec/src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
+++ API FATEC/api-fatec/src/main/java/br/com/api/fatec/apifatec/ApiFatecApplication.java
@@ -2,43 +2,36 @@ package br.com.api.fatec.apifatec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.websocket.server.PathParam;
+import java.util.Scanner;
+
 @RestController
 @SpringBootApplication
 public class ApiFatecApplication {
-	
-	@RequestMapping("/")
-	String home() {
-		return "Hello World";
-	}
-	
-	@RequestMapping("/hello")
-	String home2() {
-		return "hello word 2";
-	}
-	@RequestMapping("/teste")
-	Integer home3() {
-		return 3;
 
-	}
-	@RequestMapping("/soma")
-	Double home4() {
-		return 2.2+2;
+    @RequestMapping("/classificarIdade")
+    String classificarIdade(@RequestParam(value = "idade", required = true) Integer idade) {
+        String categoria;
+        if (idade < 0) {
+            categoria = "Idade inválida";
+        } else if (idade < 12) {
+            categoria = "Criança";
+        } else if (idade <= 18) {
+            categoria = "Adolescente";
+        } else if (idade <= 60) {
+            categoria = "Adulto";
+        } else {
+            categoria = "Idoso";
+        }
 
-	}
-	@RequestMapping("/numero/{num}")
-	Integer numero2(@PathVariable Integer num) {
-		return num;
+     
+        return "Sua categoria é: " + categoria;
+    }
 
-	}
-	
-	
-	public static void main(String[] args) {
-		SpringApplication.run(ApiFatecApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiFatecApplication.class, args);
+    }
 }
