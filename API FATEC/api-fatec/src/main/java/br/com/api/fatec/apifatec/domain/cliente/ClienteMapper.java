094a7758eb3b8b0153facc90f304b3215e02605d
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-03-21 20:41:28 -0300

    Update arquivos

---
+++ API FATEC/api-fatec/src/main/java/br/com/api/fatec/apifatec/domain/cliente/ClienteMapper.java
@@ -0,0 +1,32 @@
+package br.com.api.fatec.apifatec.domain.cliente;
+
+import br.com.api.fatec.apifatec.entities.Cliente;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ClienteMapper {
+    public static Cliente toEntity(ClienteDTO dto) {
+        Cliente cliente = new Cliente();
+        cliente.setId(dto.getId());
+        cliente.setNome(dto.getNome());
+        cliente.setRazaoSocial(dto.getRazaoSocial());
+        cliente.setEndereco(dto.getEndereco());
+        cliente.setEmail(dto.getEmail());
+        return cliente;
+    }
+
+    public static ClienteDTO toDTO(Cliente cliente) {
+        ClienteDTO dto = new ClienteDTO();
+        dto.setId(cliente.getId());
+        dto.setNome(cliente.getNome());
+        dto.setRazaoSocial(cliente.getRazaoSocial());
+        dto.setEndereco(cliente.getEndereco());
+        dto.setEmail(cliente.getEmail());
+        return dto;
+    }
+
+    public static List<ClienteDTO> toDTOList(List<Cliente> clientes) {
+        return clientes.stream().map(ClienteMapper::toDTO).collect(Collectors.toList());
+    }
+}
