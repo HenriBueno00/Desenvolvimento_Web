3eac49dc87b6040db4c0ea743fe6be0b6c2359b4
Henrique Bueno Foseca <108840429+HenriBueno00@users.noreply.github.com>
2024-03-27 19:44:23 -0300

    Update ClienteController.java

--- API FATEC/api-fatec/src/main/java/br/com/api/fatec/apifatec/controllers/ClienteController.java
+++ API FATEC/api-fatec/src/main/java/br/com/api/fatec/apifatec/controllers/ClienteController.java
@@ -1,7 +1,7 @@
 package br.com.api.fatec.apifatec.controllers;
-
+ 
 import java.util.List;
-
+ 
 import br.com.api.fatec.apifatec.domain.cliente.ClienteService;
 import br.com.api.fatec.apifatec.domain.cliente.ClienteMapper;
 import br.com.api.fatec.apifatec.domain.cliente.ClienteDTO;
@@ -16,35 +16,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
+ 
+ 
 @RestController()
 @RequestMapping("/api/clientes/v1")
 public class ClienteController {
 	
 	@Autowired
 	private ClienteService clienteService;
-
+ 
 	@GetMapping
 	public ResponseEntity<List<ClienteDTO>> listarClientes() {
 		List<ClienteDTO> clientes = ClienteMapper.toDTOList(clienteService.listarClientes());
 		return new ResponseEntity<>(clientes, HttpStatus.OK);
 	}
-
+ 
 	@GetMapping("/{id}")
 	public ResponseEntity<ClienteDTO> encontrarClientePorId(@PathVariable Long id) {
 		ClienteDTO cliente = ClienteMapper.toDTO(clienteService.encontrarClientePorId(id));
 		return cliente != null ? new ResponseEntity<>(cliente, HttpStatus.OK)
 				: new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-
+ 
 	@PostMapping
 	public ResponseEntity<ClienteDTO> salvarCliente(@RequestBody ClienteDTO clienteDTO) {
 		Cliente cliente = ClienteMapper.toEntity(clienteDTO);
 		ClienteDTO clienteSalvo = ClienteMapper.toDTO(clienteService.salvarCliente(cliente));
 		return new ResponseEntity<>(clienteSalvo, HttpStatus.CREATED);
 	}
-
+	
+	
+ 
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> deletarCliente(@PathVariable Long id) {
 		clienteService.deletarCliente(id);
